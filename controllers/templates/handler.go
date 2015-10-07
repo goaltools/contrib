de6a4dc9a0d016d5700a269c1384cf8d8b51405b
Alexander Kucherenko <alkchr@colegion.com>
2015-10-07 15:34:47 +0600

    Moved controllers from main repo

---
+++ controllers/templates/handler.go
@@ -0,0 +1,37 @@
+package templates
+
+import (
+	"log"
+	"net/http"
+)
+
+// Handler is a template handler that implements http.Handler interface.
+type Handler struct {
+	context  map[string]interface{} // Variables to be passed to the template.
+	template string                 // Path to the template to be rendered.
+	status   int                    // Expected status code of the response.
+}
+
+// Apply writes to response the result received from action.
+func (t *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Set status of the response.
+	if t.status == 0 {
+		t.status = http.StatusOK
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	// If required template exists, execute it.
+	if tpl, ok := templates[t.template]; ok {
+		w.WriteHeader(t.status)
+		err := tpl.ExecuteTemplate(w, templateName, t.context)
+		if err != nil {
+			go log.Println(err)
+		}
+		return
+	}
+
+	// Otherwise, show internal server error.
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+	go log.Printf(`Template "%s" does not exist.`, t.template)
+}
