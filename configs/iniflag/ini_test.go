f873ac212024b37990b4937e53df0fb22f5bf0a5
alconaut <alconautgh@yandex.com>
2015-10-16 14:22:12 +0600

    Implemented get method of iniflag's config

--- configs/iniflag/ini_test.go
+++ configs/iniflag/ini_test.go
@@ -1,6 +1,8 @@
 package iniflag
 
 import (
+	"fmt"
+	"go/build"
 	"reflect"
 	"testing"
 )
@@ -27,23 +29,53 @@ func TestConfigParse(t *testing.T) {
 	assertNil(t, err)
 
 	exp := map[string]map[string]string{
-		"": map[string]string{
+		"": {
 			"key1": "value1",
 			"key2": "value3",
 		},
-		"some_section1": map[string]string{
+		"some_section1": {
 			"key1": "value1",
 			"key2": "value2",
 		},
-		"some_section2": map[string]string{
+		"some_section2": {
 			"key1": "value3",
 		},
-		"some_section3": map[string]string{
+		"some_section3": {
 			"key1": "value2",
 		},
 	}
 	if !reflect.DeepEqual(exp, c.body) {
 		t.Errorf("Incorrectly parsed file. Expected:\n%v.\nGot:\n%v.", exp, c.body)
+	}
+}
+
+func TestConfigGet(t *testing.T) {
+	c := &config{
+		body: map[string]map[string]string{
+			"": {
+				"key1": "value1",
+			},
+			"paths": {
+				"xxx": "${GOPATH} - ${GOPATH}",
+			},
+		},
+	}
+	for a, vs := range map[string]struct {
+		Value string
+		Found bool
+	}{
+		"key1":        {Value: "value1", Found: true},
+		"paths:xxx":   {Value: fmt.Sprintf("%[1]s - %[1]s", build.Default.GOPATH), Found: true},
+		"section:key": {Found: false},
+		"":            {Found: false},
+		"paths:zzz":   {Found: false},
+	} {
+		if v, f := c.get(parseArgName(a)); v != vs.Value || f != vs.Found {
+			t.Errorf(
+				`Requested "%s". Expected "%s", "%v"; got "%s", "%v".`,
+				a, vs.Value, vs.Found, v, f,
+			)
+		}
 	}
 }
 
