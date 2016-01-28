d9c89d409aee8f2024653e767ebecf217e2254b1
alconaut <alconautgh@yandex.com>
2016-01-29 01:40:39 +0600

    Fix broken test of sessions controller

--- controllers/sessions/sessions_test.go
+++ controllers/sessions/sessions_test.go
@@ -1,11 +1,12 @@
 package sessions
 
 import (
+	"net/url"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
-	Init()
+	Init(url.Values{})
 	if s == nil {
 		t.Errorf("Securecookie expected to be initialized, but it is empty.")
 	}
