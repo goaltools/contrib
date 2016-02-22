74b1430e69f76385345a44018f0c6275c9885d27
alconaut <alconautgh@yandex.com>
2016-02-22 14:14:07 +0600

    Implemented pluggable template systems

--- controllers/templates/standard/template.go
+++ controllers/templates/standard/template.go
@@ -26,6 +26,11 @@ package standard
 
 import (
 	"flag"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/colegion/contrib/controllers/global"
 )
 
 var (
@@ -41,3 +46,62 @@ var (
 
 	contType = flag.String("templates:content.type", "text/html; charset=utf-8", "Content-Type header's value")
 )
+
+// Template implements (github.com/colegion/contrib/controllers/templates).Template interface.
+type Template struct {
+}
+
+// Funcs gets a map of template functions and sets them for use
+// by Init function. This must be called before Init (e.g. from your init function)
+// in order to make effect.
+func (t Template) Funcs(fm template.FuncMap) {
+	funcs = fm
+}
+
+// RenderTemplate gets a context, template name and returns a handler that may be
+// used for its rendering. If the requested template does not exist,
+// an error handler is returned.
+func (t Template) RenderTemplate(context global.Object, name string) http.Handler {
+	status := context.Status()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set necessary headers of the response.
+		w.Header().Set("Content-Type", *contType)
+
+		// If required template does exist, render it.
+		err := fmt.Errorf(`template "%s" does not exist`, name)
+		if tpl, ok := templates[name]; ok {
+			w.WriteHeader(status)
+			err = tpl.ExecuteTemplate(w, *mainBlock, context)
+			if err == nil { // Exit if execution has finished successfully.
+				return
+			}
+		}
+
+		// If the error above occured while rendering an error 500 page,
+		// log the error and show an empty page to the user.
+		if status == http.StatusInternalServerError {
+			http.Error(w, err.Error(), status)
+			return
+		}
+
+		// Otherwise, try to render 500 error page
+		// by throwing a panic that must be caught by a router.
+		panic(err.Error())
+	})
+}
+
+// Render transforms controller and action to a template name using a pattern
+// defined by "default.pattern" flag. For example, it may look as follows:
+//	fmt.Sprintf("%s/%s.html", controller, action)
+// It then calls RenderTemplate.
+func (t Template) Render(context global.Object, controller, action string) http.Handler {
+	return t.RenderTemplate(context, fmt.Sprintf(*defTplPattern, controller, action))
+}
+
+// Init starts parsing of templates.
+func (t Template) Init() {
+	templates = load(*views)
+}
+
+// templates is a set of all registered and parsed templates.
+var templates map[string]*template.Template
--- controllers/templates/templates.go
+++ controllers/templates/templates.go
@@ -1,38 +1,47 @@
-// Package templates provides abstractions for work
-// with standard Go template engine.
 package templates
 
 import (
-	"flag"
-	"fmt"
 	"html/template"
 	"net/http"
 	"net/url"
 
 	"github.com/colegion/contrib/controllers/global"
+	"github.com/colegion/contrib/controllers/requests"
+	"github.com/colegion/contrib/controllers/templates/standard"
 )
 
-var (
-	layoutTpl   = flag.String("templates:layout.file", "Layout.html", "name of the layout template file")
-	layoutBl    = flag.String("templates:layout.block", "layout", "name of the template block to render")
-	elemTplPref = flag.String("templates:element.prefix", "_", "prefix of a template containing view elements")
+// TemplateSystem is a template engine that is used by this controller.
+// By default it is a wrapper around standard templates + some conventions.
+// This may be replaced by a template system of your choice:
+//	func init() {
+//		templates.TemplateSystem = myTSThatImplementTemplateInterface
+//	}
+// If you want to set template functions, do the following:
+//	templates.TemplateSystem.Funcs(template.FuncMap{
+//		...
+//	})
+var TemplateSystem = Template(standard.Template{})
 
-	delimLeft  = flag.String("templates:delimLeft", "{%", "left action delimiter")
-	delimRight = flag.String("templates:delimRight", "%}", "right action delimiter")
+// Template is an interface that must be implemented by template systems
+// in order to be compatible with Templates controller.
+type Template interface {
+	// Funcs gets functions that will be available for use in the templates. The functions
+	// must return just 1 argument or 2 in case the second one is of error type.
+	Funcs(template.FuncMap)
 
-	views  = flag.String("templates:path", "./views/", "path to the directory with views")
-	defTpl = flag.String("templates:default.pattern", "%v/%v.html", "default template's name pattern")
+	// Init is a function that must initialize the template system,
+	// load and parse templates, etc.
+	Init()
 
-	contType = flag.String("templates:content.type", "text/html; charset=utf-8", "Content-Type header's value")
+	// RenderTemplate gets a context, template name and must return a
+	// handler that would execute the template or error 500.
+	RenderTemplate(context global.Object, name string) http.Handler
 
-	// Funcs are added to the template's function map.
-	// Functions are expected to return just 1 argument or
-	// 2 in case the second one is of error type.
-	Funcs = template.FuncMap{}
-
-	// templates is a set of all registered and parsed templates.
-	templates map[string]*template.Template
-)
+	// Render is an equivalent of RenderTemplate except instead of
+	// template name it gets current controller and action names
+	// and renders a template associated with them.
+	Render(context global.Object, controller, action string) http.Handler
+}
 
 // Templates is a controller that provides support of HTML result
 // rendering to your application.
@@ -41,65 +50,22 @@ var (
 type Templates struct {
 	// Global brings a Context that is used for passing variables to templates.
 	*global.Global
-}
 
-// After renders the result template and writes it to the response.
-func (c *Templates) After() http.Handler {
-	// Check whether template rendering was requested.
-	path := c.Context.Template()
-	if path == "" {
-		return nil
-	}
-	status := c.Context.Status()
-
-	// If so, render the requested template.
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set necessary headers of the response.
-		w.Header().Set("Content-Type", *contType)
-
-		// If required template does exist, execute it.
-		err := fmt.Errorf(`template "%s" does not exist`, path)
-		if tpl, ok := templates[path]; ok {
-			w.WriteHeader(status)
-			err = tpl.ExecuteTemplate(w, *layoutBl, c.Context)
-			if err == nil { // Exit if execution has finished successfully.
-				return
-			}
-		}
-
-		// If the error above occured while rendering an error 500 page,
-		// log the error and show an empty page to the user.
-		if status == http.StatusInternalServerError {
-			http.Error(w, err.Error(), status)
-			return
-		}
-
-		// Otherwise, try to render 500 error page
-		// by throwing a panic that must be caught by a router.
-		panic(err.Error())
-	})
+	*requests.Requests
 }
 
 // RenderTemplate is an action that gets a path to template
-// and saves it to Context for later rendering.
+// and returns an HTTP handler for its rendering.
 func (c *Templates) RenderTemplate(templatePath string) http.Handler {
-	c.Context.SetTemplate(templatePath)
-	return nil
+	return TemplateSystem.RenderTemplate(c.Context, templatePath)
 }
 
-// Render is an equivalent of the following:
-//	RenderTemplate(CurrentController + "/" + CurrentAction + ".html")
-// The default path pattern may be overriden by adding the following
-// line to your configuration file:
-//	[templates]
-//	default.pattern = %s/%s.tpl
+// Render executes a te
 func (c *Templates) Render() http.Handler {
-	return c.RenderTemplate(
-		fmt.Sprintf(*defTpl, c.CurrentController, c.CurrentAction),
-	)
+	return TemplateSystem.Render(c.Context, c.CurrentController, c.CurrentAction)
 }
 
 // Init triggers loading of templates.
 func Init(url.Values) {
-	templates = load(*views)
+	TemplateSystem.Init()
 }
